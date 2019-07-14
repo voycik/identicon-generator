fb60e5a7b4093dbcbe9ba0dce48d201bdb1478bd
voycik <voycik@gmail.com>
2019-07-14 10:46:51 +0200

    Get RGB color from hex list

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,12 +1,19 @@
 defmodule Identicon do
-def main(input) do
-  input
-  |> hash_input
-end
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
 
-def hash_input(input) do
-  hex = :crypto.hash(:md5, input)
-  |> :binary.bin_to_list
-  %Identicon.Image{hex: hex}
-end
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g, b | _tail] = hex_list
+    [r, g, b]
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
+  end
 end
