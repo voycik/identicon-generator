2c8ede99b90705d1c0490a9697157ba7979cdf21
voycik <voycik@gmail.com>
2019-07-14 16:42:27 +0200

    Generate and save identicon image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,8 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   def pick_color(image) do
@@ -45,13 +47,25 @@ defmodule Identicon do
     pixel_map =
       Enum.map grid, fn({_code, index}) ->
         horizontal = rem(index, 5) * 50
-        vertical = div(index, 5) * 5
+        vertical = div(index, 5) * 50
         top_left = {horizontal, vertical}
         bottom_right = {horizontal + 50, vertical + 50}
         {top_left, bottom_right}
       end
 
     %Identicon.Image{ image | pixel_map: pixel_map}
+  end
 
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+    :egd.render(image)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
   end
 end
