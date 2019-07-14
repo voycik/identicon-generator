34ac8e40330a639abc8d5dfcf59408f9fa190e56
voycik <voycik@gmail.com>
2019-07-14 15:02:53 +0200

    Filter blank squares

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,6 +4,7 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
   end
 
   def pick_color(image) do
@@ -18,12 +19,24 @@ defmodule Identicon do
   end
 
   def build_grid(%Identicon.Image{hex: hex_list} = image) do
-    hex_list
-    |> Enum.chunk_every(3, 3, :discard)
-    |> Enum.map(&mirror_row/1)
+    grid =
+      hex_list
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+    %Identicon.Image{ image | grid: grid}
   end
 
   def mirror_row([first, second | _tail] = row) do
     row ++ [second, first]
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid =
+      Enum.filter grid, fn({code, _index}) ->
+        rem(code, 2) == 0
+      end
+    %Identicon.Image{ image | grid: grid}
   end
 end
