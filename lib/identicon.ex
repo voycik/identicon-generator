9ad87005126c33caf01aa356d6b7a75f71a907a1
voycik <voycik@gmail.com>
2019-07-14 16:00:40 +0200

    Build pixel map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,7 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
   end
 
   def pick_color(image) do
@@ -38,5 +39,19 @@ defmodule Identicon do
         rem(code, 2) == 0
       end
     %Identicon.Image{ image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map grid, fn({_code, index}) ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 5
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+        {top_left, bottom_right}
+      end
+
+    %Identicon.Image{ image | pixel_map: pixel_map}
+
   end
 end
